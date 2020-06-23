8b87238a0e8be197b4ebdd0f37924cb4e969afd6
Maegar <proske.paul@googlemail.com>
2020-06-23 10:23:17 +0200

    Refactoring and documentation added

--- lupusec_parser/__init__.py
+++ lupusec_parser/__init__.py
@@ -13,74 +13,128 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""LUPUSEC UI extractor.
+
+This module is able to recognize HTTP requests and response pairs for a LUPUSEC UI
+(alarm-panel). All important data will be printed to the console. This is typically
+needed for reverse engineering of LUPUSEC alarm panels. To do so chromedev tools
+will be used - therefore chrome must be started in debugging mode before
+e.g. /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222
+
+Port is set fix to 9222
+
+  Typical usage example:
+
+  lupusec_parser.gatherInformation(lupusec_url, lupusec_username, lupusec_password)
+"""
+
 import pychrome
 import pychrome.exceptions
 import base64
 import json
 
-def gatherInformation(url, username, password, time):
-    # create a browser instance
-    browser = pychrome.Browser(url="http://127.0.0.1:9222")
+class LupusecExtractor:
+    """Lupusec extractor.
 
-    # create a tab
-    tab = browser.new_tab()
+    This extractor contains all functions to capture / extract HTTP request and
+    response pairs.
 
-    requests = {}
-    
-    # register callback if you want
-    def request_will_be_sent(**kwargs):
-        reqId = kwargs.get('requestId')
+    Attributes:
+        result: Dictionary with all details for all captured request and response pairs 
+    """
+
+    def __init__(self, url, username, password):
+        """ Initialise the class 
+        
+        Args:
+            url: The Lupusec URL.
+            username: Username for login to the Lupusec UI
+            password: Password for login to the Lupusec UI
+        """
+        self._url = url
+        self._username = username
+        self._password = password
+        self._browser = pychrome.Browser(url="http://127.0.0.1:9222")
+        self.result = {}
+
+    def _capture_request(self, **kwargs):
+        """ Capture outgoing XHR request """
         if kwargs.get('type') == 'XHR':
-            request = {'method': kwargs.get('request').get('method'),
-                       'header_accept': kwargs.get('request').get('headers').get('Accept')}
-
-            if kwargs.get('request').get('method') == 'POST':
-                data = tab.Network.getRequestPostData(requestId=reqId)
+            http_request = kwargs.get('request')
+            request = {'method': http_request.get('method'),
+                    'header_accept': http_request.get('headers').get('Accept')}
+            
+            reqId = kwargs.get('requestId')
+            if http_request.get('method') == 'POST':
+                data = self._tab.Network.getRequestPostData(requestId=reqId)
                 request['postData'] = data
-            
-            requests[reqId] = {'request': request, 'url': kwargs.get('request').get('url')}
+
+            self.result[reqId] = {'request': request, 'url': http_request.get('url')}
     
     
-    # register callback if you want
-    def response_receied(**kwargs):
+    def _capture_response(self, **kwargs):
+        """ Capture incoming XHR response """
         if kwargs.get('type') == 'XHR':
-            response = {'mime_type': kwargs.get('response').get('mimeType'),
-                        'status': kwargs.get('response').get('status'),
-                        'status_text': kwargs.get('response').get('statusText')}
+
+            http_response = kwargs.get('response')
+            response = {'mime_type': http_response.get('mimeType'),
+                        'status': http_response.get('status'),
+                        'status_text': http_response.get('statusText')}
             
-            requests[kwargs.get('requestId')]['response'] = response
+            self.result[kwargs.get('requestId')]['response'] = response
 
-    def loading_finished(**kwargs):
+    def _capture_finished_loaded_response(self, **kwargs):
+        """ Capture finish loaded XHR response body """
         reqId = kwargs.get('requestId')
-        if reqId in requests:
-            bodyResp = tab.Network.getResponseBody(requestId=reqId)
-            requests[kwargs.get('requestId')]['request']['body'] = bodyResp
+        if reqId in self.result:
+            bodyResp = self._tab.Network.getResponseBody(requestId=reqId)
+            self.result[kwargs.get('requestId')]['request']['body'] = bodyResp['body'].replace('\n', '').replace('\t', '')
 
-    tab.Network.requestWillBeSent = request_will_be_sent
-    tab.Network.responseReceived = response_receied
-    tab.Network.loadingFinished = loading_finished
+    def _continue_requests(self, **kwargs):
+        """ Continue the request """
+        self._tab.Fetch.continueRequest(requestId=kwargs['requestId'])
 
+    def _do_auth(self, **kwargs):
+        """ Do the authorization """
+        self._tab.Fetch.continueWithAuth(requestId=kwargs['requestId'], authChallengeResponse={'response': 'ProvideCredentials', 'username': self._username, 'password': self._password})
+        self._tab.Fetch.disable()
 
-    def continue_requests(**kwargs):
-        tab.Fetch.continueRequest(requestId=kwargs['requestId'])
+    def gatherInformation(self, timeout):
+        """ Collect information from Lupusec UI
 
-    def do_auth(**kwargs):
-        tab.Fetch.continueWithAuth(requestId=kwargs['requestId'], authChallengeResponse={'response': 'ProvideCredentials', 'username': username, 'password': password})
-        tab.Fetch.disable()
+        Open a new tab in the remote debugging chrome to access Lupusec UI and
+        capture all request response pairs that will be automatically loaded
 
-    tab.Fetch.authRequired = do_auth
-    tab.Fetch.requestPaused = continue_requests
+        Args:
+            timeout: Seconds how long we will wait for collection informations
+        
+        Returns:
+            A dict mapping request IDs to the corresponding XHR response / requests pairs.
+            Each tuple contains a request and response pair in a dict format.
+        """
+        self._tab = self._browser.new_tab()
+
+        self._tab.Network.requestWillBeSent = self._capture_request
+        self._tab.Network.responseReceived = self._capture_response
+        self._tab.Network.loadingFinished = self._capture_finished_loaded_response
+
+        self._tab.Fetch.authRequired = self._do_auth
+        self._tab.Fetch.requestPaused = self._continue_requests
+        
+        self._tab.start()
+        self._tab.Network.enable()
+        self._tab.Fetch.enable(handleAuthRequests=True)
+        self._tab.Security.setIgnoreCertificateErrors(ignore=True)
+        self._tab.Page.navigate(url=self._url, _timeout=timeout)
+        self._tab.wait(timeout)
+        self._tab.stop()
+        self._browser.close_tab(self._tab)
+
+        return self.result
     
-    tab.start()
-    tab.Network.enable()
-    tab.Fetch.enable(handleAuthRequests=True)
-    tab.Security.setIgnoreCertificateErrors(ignore=True)
-    tab.Page.navigate(url=url, _timeout=time)
-    tab.wait(time)
-    tab.stop()
-    browser.close_tab(tab)
-
-    for x, y in requests.items():
-        print('--------------- ' + y['url'] + ' ---------------')
-        print(json.dumps(y, indent=4, sort_keys=True))
-        print('------------------------------------------\n')
+    def print_result(self):
+        """ Print the result to console """
+        for y in self.result.values():
+            print('--------------- ' + y['url'] + ' ---------------')
+            print(json.dumps(y, indent=4, sort_keys=True))
+            print('------------------------------------------\n')
--- lupusec_parser/__main__.py
+++ lupusec_parser/__main__.py
@@ -13,36 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""LUPUSEC UI extractor CLI module.
+
+This module is the CLI for the LupusecExtractor 
+"""
+
 import lupusec_parser
 import argparse
 
 def get_arguments():
+    """ Get arguments that we need for CLI """
     parser = argparse.ArgumentParser("LupusecParser: Command Line Utility")
-    _add_argument(parser, '-u', '--username', 'Username for Lupusec UI')
-    _add_argument(parser, '-p', '--password', 'Password for Lupusec UI')
-    _add_argument(parser, '-a', '--address', 'Lupus panel http Lupusec adress')
-
-    parser.add_argument(
-        '-t', '--time',
-        help='time until the recognition stops',
-        type=int,
-        required=False,
-        default=5)
+    parser.add_argument('-u', '--username', help='LUsername for Lupusec UI', required=True)
+    parser.add_argument('-p', '--password', help='Password for Lupusec UI', required=True)
+    parser.add_argument('-a', '--address', help='Lupus panel http Lupusec adress', required=True)
+    parser.add_argument('-t', '--time', help='time until the recognition stops', type=int, required=False, default=5)
     return parser.parse_args()
 
-def _add_argument(parser, argument, argument_long, help, required=True):
-    parser.add_argument(
-        argument, argument_long,
-        help=help,
-        required=required)
-
-def call():
-    args = get_arguments()
-    lupusec_parser.gatherInformation(args.address, args.username, args.password, args.time)
-  
-
 def main():
-    call()
+    """ Main method that will be used if module is started through a command line """
+    args = get_arguments()
+    extractor = lupusec_parser.LupusecExtractor(args.address, args.username, args.password)
+    extractor.gatherInformation(args.time)
+    extractor.print_result()
 
 if __name__ == '__main__':
     main()
